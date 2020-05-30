eb508c748e19123b4eee68c6ec20558f5d8e8a88
navdeepkaurravala <navdeep.ravala18@gmail.com>
2020-05-30 19:11:57 +0530

    new changes to develop branch

--- src/main/java/MyLearning/MyNewTest.java
+++ src/main/java/MyLearning/MyNewTest.java
@@ -5,6 +5,7 @@ public class MyNewTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Navdeep Kaur");
+System.out.println("lalalallal");
 	}
 
 }
